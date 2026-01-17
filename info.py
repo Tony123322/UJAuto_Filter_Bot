0f3f1b6ba0ac87dfee0fcc41a50840c7e73473d2
Tony123322 <141171857+Tony123322@users.noreply.github.com>
2026-01-17 11:36:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -126,7 +126,7 @@ THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "54000"))
 # ============================
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+gHZnWypXWqM5NzBl') # Group link for the bot
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/dreamxbotz') # Owner link for the bot
-UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/dreamxbotz') # Update channel link for the bot
+UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/AapkaFreeTV') # Update channel link for the bot
 
 # ============================
 # User Configuration
