ce0efe905a554aa7d6a9a9f6da0a1e7da85dad13
Tony123322 <141171857+Tony123322@users.noreply.github.com>
2026-01-20 23:35:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -45,7 +45,7 @@ FSUB_PICS = (environ.get('FSUB_PICS', 'https://graph.org/file/7478ff3eac37f4329c
 # ============================
 # Admin, Channels & Users Configuration
 # ============================
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '8123573920').split()] # Replace with the actual admin ID(s) to add
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7871622127').split()] # Replace with the actual admin ID(s) to add
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1003638356541 -1003601239756').split()]  # Channel id for auto indexing (make sure bot is admin)
 
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1003282437476'))  # Log channel id (make sure bot is admin)
